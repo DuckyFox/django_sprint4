b17a16248f8f2344cc6354f2c7b16e4cf57e8379
Dmitry Yakimov <Diman.yackim@ya.ru>
2024-03-14 16:31:00 +0400

    duckfor

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     path('auth/registration', CreateView.as_view(
         template_name='registration/registration_form.html',
         form_class=UserCreationForm,
-        success_url=reverse_lazy('blog:index'),),
-         name='registration')
+        success_url=reverse_lazy('blog:index')
+        ), name='registration')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
