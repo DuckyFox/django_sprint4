5dffc17a59a4613990c460dd467293b801230c64
Dmitry Yakimov <Diman.yackim@ya.ru>
2024-03-14 16:34:00 +0400

    duckfor

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -16,11 +16,12 @@ urlpatterns = [
     path('pages/', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
-    path('auth/registration', CreateView.as_view(
-        template_name='registration/registration_form.html',
-        form_class=UserCreationForm,
-        success_url=reverse_lazy('blog:index')
-        ), name='registration')
+    path('auth/registration',
+         CreateView.as_view(
+                            template_name='registration/registration_form.html',
+                            form_class=UserCreationForm,
+                            success_url=reverse_lazy('blog:index')
+                            ), name='registration')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
